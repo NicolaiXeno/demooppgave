a5736055442df011c3e65fc3178edb3a16d0179b
68455576 <nicolaigjelsvik@outlook.com>
2022-10-07 09:21:15 +0200

    fix

--- laksetall/main.py
+++ laksetall/main.py
@@ -2,7 +2,7 @@ Input = ''
 while not Input.isdigit() and not len(Input) == 1:
     Input = input('Oppgi en tall: ')
 
-for i in range(1,Input+1):
+for i in range(1,float(Input)+1):
     if i > 1:
         print('{} lakser'.format(i))
     else:
